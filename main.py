afef53c5a52f07a1ea58303ee3ed2adf85101e7b
Asadbek <asadbekakhtamov20@gmail.com>
2025-01-25 14:14:39 +0500

    first part

---
+++ main.py
@@ -0,0 +1,31 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler
+import os
+
+like_count = 0
+dislike_count = 0
+
+
+def start(update, context):
+
+    photo = open("image.png", "rb")
+
+    keyboard = [
+        [InlineKeyboardButton(f"👍 Like {like_count}", callback_data="like")],
+        [InlineKeyboardButton(f"👎  Dislike {dislike_count}", callback_data="dislike")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_photo(photo=photo, reply_markup=reply_markup)
+
+
+TOKEN = os.getenv("TOKEN")
+
+updater = Updater(TOKEN) 
+
+dispatcher = updater.dispatcher
+
+
+dispatcher.add_handler(CommandHandler("start", start))
+
+updater.start_polling()
+updater.idle()
